4985f17cad0f84cbf13e22e13aec6e363d40ebc9
Michael Ko <michael@faide.net>
2023-06-01 21:42:57 -0600

    Install tailwind, add redraw button, and set up for more noise methods in the future

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,62 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import { start, stop } from "./draw";
+import { draw } from "./draw";
 
 function App() {
   const canvasRef = useRef(null);
+  const [method, setMethod] = useState<
+    "midpointDisplacement" | "diamondSquare"
+  >("midpointDisplacement");
 
   useEffect(() => {
-    console.log("starting");
-    if (canvasRef.current) start(canvasRef.current);
-
-    return () => {
-      stop();
-    };
+    if (canvasRef.current) draw(canvasRef.current, "midpointDisplacement");
   }, []);
+
+  const handleRedraw = () => {
+    if (canvasRef.current) draw(canvasRef.current, method);
+  };
   return (
-    <>
-      <canvas width={1000} height={1000} id="canvas" ref={canvasRef} />
-    </>
+    <div className="flex flex-col gap-4 my-4">
+      <div className="flex flex-row justify-center gap-8 text-white items-center">
+        <label htmlFor="midpointDisplacement">
+          Midpoint displacement
+          <input
+            type="radio"
+            name="method"
+            id="midpointDisplacement"
+            value="midpointDisplacement"
+            checked={method === "midpointDisplacement"}
+            onChange={() => setMethod("midpointDisplacement")}
+          />
+        </label>
+        {/*<label htmlFor="diamondSquare">
+          Diamond Square
+          <input
+            type="radio"
+            name="method"
+            value="diamondSquare"
+            id="diamondSquare"
+            checked={method === "diamondSquare"}
+            onChange={() => setMethod("diamondSquare")}
+          />
+        </label>*/}
+        <button
+          onClick={handleRedraw}
+          className="bg-green-600 px-4 py-2 rounded hover:bg-green-800 hover:cursor-pointer"
+        >
+          Redraw
+        </button>
+      </div>
+      <div>
+        <canvas
+          className="mx-auto"
+          width="1000px"
+          height="1000px"
+          id="canvas"
+          ref={canvasRef}
+        />
+      </div>
+    </div>
   );
 }
 
--- src/draw.ts
+++ src/draw.ts
@@ -1,37 +1,25 @@
-let ctx: CanvasRenderingContext2D | null;
-let width = 0;
-let height = 0;
+export function draw(
+  canvasEl: HTMLCanvasElement,
+  method: "midpointDisplacement" | "diamondSquare"
+) {
+  const ctx = canvasEl.getContext("2d");
+  const width = canvasEl.clientWidth;
+  const height = canvasEl.clientHeight;
 
-// let running = false;
-// let timer = 0;
-
-let heights: number[][];
-
-export function start(canvasEl: HTMLCanvasElement) {
-  ctx = canvasEl.getContext("2d");
-
-  if (ctx === null) return;
-
-  width = canvasEl.clientWidth;
-  height = canvasEl.clientHeight;
-
-  const start = performance.now();
-  heights = midpointDisplacement2D(width, height, 1000, 0.8);
-  normalizeHeightMap(heights, 255);
-  const time = performance.now() - start;
-  console.log(`terrain generation took ${time}ms`);
-
-  draw();
-  // window.requestAnimationFrame(() => draw());
-}
-
-export function stop() {
-  // running = false;
-}
-
-function draw() {
   if (!ctx || width === 0 || height === 0) return;
-  const start = performance.now();
+
+  const tgenStart = performance.now();
+  let heights: number[][] = [];
+  if (method === "midpointDisplacement") {
+    heights = midpointDisplacement2D(width, height, 1000, 0.8);
+  } else if (method === "diamondSquare") {
+    heights = diamondSquare(width, height, 1000, 0.8);
+  }
+  normalizeHeightMap(heights, 255);
+  const tgenTime = performance.now() - tgenStart;
+  console.log(`terrain generation took ${tgenTime}ms`);
+
+  const renderStart = performance.now();
 
   const buffer = ctx.createImageData(width, height);
 
@@ -48,10 +36,8 @@ function draw() {
 
   ctx.putImageData(buffer, 0, 0);
 
-  const time = performance.now() - start;
-  console.log(`frame took ${time}ms`);
-
-  // if (running) window.requestAnimationFrame(() => draw());
+  const renderTime = performance.now() - renderStart;
+  console.log(`frame render took ${renderTime}ms`);
 }
 
 function normalizeHeightMap(heightMap: number[][], max: number) {
@@ -76,6 +62,87 @@ function normalizeHeightMap(heightMap: number[][], max: number) {
 }
 
 function midpointDisplacement2D(
+  width: number,
+  height: number,
+  maxRand: number,
+  decayFactor: number
+): number[][] {
+  const heightMap: number[][] = new Array(height);
+  for (let y = 0; y < heightMap.length; y++) {
+    heightMap[y] = new Array(width).fill(0);
+  }
+
+  // put random values in the corners
+  // top left
+  heightMap[0][0] = Math.random() * maxRand;
+  // top right
+  heightMap[0][width - 1] = Math.random() * maxRand;
+  // bottom left
+  heightMap[height - 1][0] = Math.random() * maxRand;
+  // bottom right
+  heightMap[height - 1][width - 1] = Math.random() * maxRand;
+
+  // [xMin, xMax, yMin, yMax, randomness]
+  const queue: Array<[number, number, number, number, number]> = [];
+  let queueIndex = 0;
+
+  queue.push([0, width - 1, 0, height - 1, maxRand / 2]);
+
+  while (queueIndex < queue.length) {
+    const next = queue[queueIndex++];
+    if (!next) continue; // why Typescript, why
+    const [xMin, xMax, yMin, yMax, randomness] = next;
+
+    const xMid = Math.floor((xMax + xMin) / 2);
+    const yMid = Math.floor((yMax + yMin) / 2);
+
+    const halfRand = randomness / 2;
+
+    // calculate the midpoints of each edge, and add randomness
+
+    // mid-left = midpoint(topleft, bottomleft)
+    heightMap[yMid][xMin] =
+      (heightMap[yMin][xMin] + heightMap[yMax][xMin]) / 2 +
+      (Math.random() * randomness - halfRand);
+
+    // mid-right = midpoint(topright, bottomright)
+    heightMap[yMid][xMax] =
+      (heightMap[yMin][xMax] + heightMap[yMax][xMax]) / 2 +
+      (Math.random() * randomness - halfRand);
+
+    // mid-top = midpoint(topleft, topright)
+    heightMap[yMin][xMid] =
+      (heightMap[yMin][xMin] + heightMap[yMin][xMax]) / 2 +
+      (Math.random() * randomness - halfRand);
+
+    // mid-bottom = midpoint(bottomleft, bottomright)
+    heightMap[yMax][xMid] =
+      (heightMap[yMax][xMin] + heightMap[yMax][xMax]) / 2 +
+      (Math.random() * randomness - halfRand);
+
+    // center = avg(midleft, midright, midtop, midbottom)
+    const center =
+      (heightMap[yMid][xMin] +
+        heightMap[yMid][xMax] +
+        heightMap[yMin][xMid] +
+        heightMap[yMax][xMid]) /
+        4 +
+      (Math.random() * randomness - halfRand);
+
+    heightMap[yMid][xMid] = center;
+
+    // Recurse if we can find another midpoint
+    if (xMax - xMin > 2) {
+      queue.push([xMin, xMid, yMin, yMid, randomness * decayFactor]);
+      queue.push([xMid, xMax, yMin, yMid, randomness * decayFactor]);
+      queue.push([xMin, xMid, yMid, yMax, randomness * decayFactor]);
+      queue.push([xMid, xMax, yMid, yMax, randomness * decayFactor]);
+    }
+  }
+  return heightMap;
+}
+
+function diamondSquare(
   width: number,
   height: number,
   maxRand: number,
